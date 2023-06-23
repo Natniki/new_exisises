d579e4eb41cbf9a25ae4c59a51222ae0f4fcbdb5
Natallia Nikitsina <natiknik73@gmail.com>
2023-06-23 15:36:36 +0300

    task_2 solved

--- task_2/Program.cs
+++ task_2/Program.cs
@@ -1,3 +1,41 @@
 ﻿// Программа принимает на вход число и выдает количество цифр в числе.
 // 453->7, 78-*78->2
 
+
+/*using System;
+using System.Collections.Generic;
+using System.Text;*/
+
+
+int GetNumber(string deskription)
+{
+    int number;
+    Console.Write($"{deskription}=> ");
+    while(true)
+    {
+            string temp = Console.ReadLine();
+            if(int.TryParse(temp, out number))
+            {
+                return number;
+            }
+            Console.Write($"This number \"{temp}\" is not correct. Try again => ");
+    }
+}
+
+int number = GetNumber("input number: ");
+
+int CountNumber(int number)
+{
+    int count = 0;
+    while(number>0)
+    {
+        number /=10;
+        count++;
+    }
+    return count;
+
+}
+Console.WriteLine(CountNumber(number));
+
+
+
