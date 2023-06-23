b89035c9958f5de63a0ae3786a7ce0a728378563
Natallia Nikitsina <natiknik73@gmail.com>
2023-06-23 14:29:15 +0300

    added folder task_2 + conditions task

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int GetNumber(string deskription)
+{
+    int number;
+    Console.Write($"{deskription}=> ");
+    while(true)
+    {
+            string temp= Console.ReadLine();
+            if(int.TryParse(temp, out number))
+            {
+                return number;
+            }
+            Console.Write($"This number \"{temp}\" is not correct. Try again => ");
+    }
+}
+
+int GetSUM(int number)
+{
+    int Sum=0;
+    for (int i = 0; i <= number; i++)
+    {
+        Sum += i;
+    }
+    return Sum;
+}
+
+//int number=GetNumber("input number");
+//Console.WriteLine($"sum number from 1 to {number} is {GetSUM(number)}.");
+
+int [] arrey = {4, 3, 8, 9};
+
+Console.WriteLine(string.Join(", ", arrey));
