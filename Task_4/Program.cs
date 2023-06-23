a185c9101f8a9a3acd950fd7ef80d0ca093eb64d
Natallia Nikitsina <natiknik73@gmail.com>
2023-06-23 17:15:30 +0300

    all task solved

--- Task_4/Program.cs
+++ Task_4/Program.cs
@@ -3,3 +3,33 @@
 
 
 
+/*int GetNumber(string deskription)
+{
+    int number;
+    Console.Write($"{deskription}=> ");
+    while(true)
+    {
+            string temp = Console.ReadLine();
+            if(int.TryParse(temp, out number))
+            {
+                return number;
+            }
+            Console.Write($"This number \"{temp}\" is not correct. Try again => ");
+    }
+}
+int number = GetNumber("input number: ");*/
+
+void FiillArrey(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while(index<length)
+    {
+        collection[index]=new Random().Next(0, 2);
+        index++;
+    }
+    Console.WriteLine($"{string.Join(", ", collection)}");
+}
+
+int [] arr = new int[8];
+FiillArrey(arr);
