431322147e001c817727ddc0b5b104aa0fa83c5a
Natallia Nikitsina <natiknik73@gmail.com>
2023-06-23 16:17:15 +0300

    task_3 solved

---
+++ Task_3/Program.cs
@@ -0,0 +1,36 @@
+﻿// Программа , которая на вход принимает число N и выдает произведение чисел от 1 до N.
+// 4->24, 5->120
+
+/*using System;
+using System.Collections.Generic;
+using System.Text;*/
+
+int GetNumber(string deskription)
+{
+    int number;
+    Console.Write($"{deskription}=> ");
+    while(true)
+    {
+            string temp = Console.ReadLine();
+            if(int.TryParse(temp, out number))
+            {
+                return number;
+            }
+            Console.Write($"This number \"{temp}\" is not correct. Try again => ");
+    }
+}
+int number = GetNumber("input number: ");
+
+int ProductOfNumber(int number)
+{
+    int multiplication=1;
+    for(int i = 1; i <=number; i++)
+    {
+        multiplication *=i;
+    }    
+    return multiplication;
+}
+
+Console.WriteLine(ProductOfNumber(number));
+
+
